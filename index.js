36e5dfec64ac96daf313f946de61cba5902c5f19
Vladimir Sukharev aka Nirvandil <nirvandil@gmail.com>
2018-04-10 00:08:50 +0300

    Implemented. Added sleeps.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const xlsx = require('xlsx');
 const request = require('sync-request');
+const sleep = require('thread-sleep');
+
 const workBook = xlsx.readFile('/media/load/models_v_ak27_autosoft.xls');
 const targetCells = [];
 for (let i = 2; i < 1000; i++) targetCells.push('B' + i);
@@ -7,28 +9,30 @@ const unique = (item, index, self) => self.indexOf(item) === index;
 const needed = item => /^[\x00-\x7F]*$/.test(item) || item.includes('Легковые');
 const normalizeModelName = modelString => {
     const splitted = modelString.split(/,|, /);
-    //console.log(`Splitted: ${splitted[0]}`);
-    return splitted[0];
+    return splitted[0].split(' ')[0];
 };
 const sendPost = (data, address) => {
     const response = request('POST', `http://192.168.1.244/api/1.0${address}`, {
-        body: data,
+        body: JSON.stringify(data),
         gzip: false,
         headers: {
             'content-type': 'application/json',
-            'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyX2lkIl0sInVzZXJfbmFtZSI6ImFkbWluaXN0cmF0b3IiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNTIzMzI0Nzk4LCJhdXRob3JpdGllcyI6WyI5MjIzMzcyMDM2ODU0Nzc1ODA3IiwiUk9MRV9BRE1JTiJdLCJqdGkiOiJiYzg4ZGQ4Ni04ZDY4LTRlODktOTBhNi0zZTdjMDEzMGZjNzciLCJjbGllbnRfaWQiOiJhbmRyb2lkIn0.T27HOWrXjukqHmKW3dmB4OCs09FXyS2k_W1tARm0O6g',
+            'Authorization' : 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsib2F1dGgyX2lkIl0sInVzZXJfbmFtZSI6ImFkbWluaXN0cmF0b3IiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNTIzMzQ5MTM0LCJhdXRob3JpdGllcyI6WyI5MjIzMzcyMDM2ODU0Nzc1ODA3IiwiUk9MRV9BRE1JTiJdLCJqdGkiOiJlOTQ4MGE3My00ZmQwLTQ5NTAtOTVhNy04NmZmOTYyNDcxYWUiLCJjbGllbnRfaWQiOiJhbmRyb2lkIn0._EWtlvm9j_6eAQBEQ50L4NNJjJkhc0vKaOIgG0KhYrM',
         },
     });
     console.log(JSON.parse(response.getBody('utf8')));
-    return JSON.parse(response.getBody('utf8'))['id'];
+    sleep(600);
+    return JSON.parse(response.getBody('utf8'));
 };
 const createBrand = brand => {
     const normalize = str => str.split('-')[0];
-    console.log(normalize(brand));
-    return sendPost(JSON.stringify({name: normalize(brand)}), '/dictionary/car/brand');
+    console.log(`Creating brand: ${normalize(brand)}`);
+    const response = sendPost({name: normalize(brand)}, '/dictionary/car/brand');
+    return response['id'];
 };
 const createModel = request => {
-    //console.log('Creating model ' + request)
+    console.log(`Creating model ${JSON.stringify(request)}`);
+    sendPost(request, '/dictionary/car/model');
 };
 
 workBook.SheetNames
@@ -47,7 +51,6 @@ workBook.SheetNames
                 brandModelData.models.push(normalizeModelName(modelString));
             }
         });
-        console.log(brandModelData);
         return Object.freeze(brandModelData);
     })
     .forEach(brandModelData => {
@@ -55,6 +58,6 @@ workBook.SheetNames
             .filter(unique)
             .forEach(modelName => {
                 const createModelRequest = {carBrandId: brandModelData.brandId, name: modelName, seats: 4};
-                createModel(JSON.stringify(createModelRequest));
+                createModel(createModelRequest);
             })
     });
